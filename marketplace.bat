afbdca11f363544cc9824657829ee03c764c7a73
sarthaksharma1301f-ship-it <sarthaksharma1301f@gmail.com>
2026-09-19 05:35:40 +0530

    Add files via upload

---
+++ marketplace.bat
@@ -0,0 +1,8 @@
+@echo off
+title SparkGig - Creator Gig Marketplace
+echo ========================================================
+echo  Launching SparkGig - Creator Gig Marketplace (MVP)
+echo ========================================================
+echo Opening index.html in your default web browser...
+start "" "%~dp0index.html"
+echo Done! Enjoy exploring the marketplace.
